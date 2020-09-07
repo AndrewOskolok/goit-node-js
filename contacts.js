19e9ad075523e334d42976287df142d0afa49f47
Oscolok <oscolok7@gmail.com>
2020-09-07 14:49:36 +0300

    refactor my code and add error message

--- contacts.js
+++ contacts.js
@@ -3,69 +3,67 @@ const path = require("path");
 
 const contactsPath = path.join(__dirname, "./db/contats.json");
 
-module.exports.get = function listContacts() {
+module.exports.get = async function listContacts() {
   //read file
-  fs.readFile(contactsPath, "utf8", function (error, data) {
-    const allObj = JSON.parse(data);
-    //push data in console
-    console.table(allObj);
-  });
+  const data = await fs.promises.readFile(contactsPath, "utf8");
+  //parse data to obj
+  const allObj = JSON.parse(data);
+  //push data in console
+  console.table(allObj);
 };
 
-module.exports.getId = function getContactById(contactId) {
+module.exports.getId = async function getContactById(contactId) {
   //read file
-  fs.readFile(contactsPath, "utf8", function (error, data) {
-    //parse data to obj
-    const allObj = JSON.parse(data);
-    //object search by id and push data in console
-    console.table(allObj.find((el) => el.id === contactId));
-  });
+  const data = await fs.promises.readFile(contactsPath, "utf8");
+  //parse data to obj
+  const allObj = JSON.parse(data);
+  //object search by id
+  const foundUser = allObj.find((el) => el.id === contactId);
+  //push foundUser to console or error message
+  console.table(foundUser ? foundUser : "User not found");
 };
 
-module.exports.removeId = function removeContact(contactId) {
+module.exports.removeId = async function removeContact(contactId) {
   //read file
-  fs.readFile(contactsPath, "utf8", function (error, data) {
-    //parse data to obj
-    const allObj = JSON.parse(data);
-    //checking id
-    if (allObj.find((el) => el.id === contactId)) {
-      //filtered array
-      const filterObj = allObj.filter((el) => el.id !== contactId);
-      //translate the array into a string
-      const objectToString = JSON.stringify(filterObj, null, " ");
-      //write new array in file
-      fs.writeFile(contactsPath, objectToString, function (error, data) {
-        //reread the file and push in console
-        fs.readFile(contactsPath, "utf8", function (error, data) {
-          //parse data to obj
-          const forTable = JSON.parse(data);
-          console.table(forTable);
-        });
-      });
-    }
-  });
-};
-
-module.exports.addId = function addContact(name, email, phone) {
-  //read file
-  fs.readFile(contactsPath, "utf8", function (error, data) {
-    //parse data to obj
-    const allObj = JSON.parse(data);
-    //create a new array with old objects and new
-    const newObj = [
-      ...allObj,
-      { id: allObj[allObj.length - 1].id + 1, name, email, phone },
-    ];
+  const data = await fs.promises.readFile(contactsPath, "utf8");
+  //parse data to obj
+  const allObj = JSON.parse(data);
+  //checking id
+  if (isNaN(contactId)) {
+    console.log("Invalid id entry");
+  } else if (allObj.find((el) => el.id === contactId)) {
+    //filtered array
+    const filterObj = allObj.filter((el) => el.id !== contactId);
     //translate the array into a string
-    const objectToString = JSON.stringify(newObj, null, " ");
+    const objectToString = JSON.stringify(filterObj, null, " ");
     //write new array in file
-    fs.writeFile(contactsPath, objectToString, function (error, data) {
-      //reread the file and push in console
-      fs.readFile(contactsPath, "utf8", function (error, data) {
-        //parse data to obj
-        const forTable = JSON.parse(data);
-        console.table(forTable);
-      });
-    });
-  });
+    await fs.promises.writeFile(contactsPath, objectToString);
+    //push successful answer to console
+    console.table(`User ${contactId} successful delete`);
+  } else {
+    //push bad answer to console
+    console.table("User id not found");
+  }
+};
+
+module.exports.addId = async function addContact(name, email, phone) {
+  //read file
+  const data = await fs.promises.readFile(contactsPath, "utf8");
+  //parse data to obj
+  const allObj = JSON.parse(data);
+  //create user object
+  const newUserObj = {
+    id: allObj[allObj.length - 1].id + 1,
+    name,
+    email,
+    phone,
+  };
+  //add newUserObject to userArrey
+  const newObj = [...allObj, newUserObj];
+  //translate the array into a string
+  const objectToString = JSON.stringify(newObj, null, " ");
+  //write new array in BD
+  await fs.promises.writeFile(contactsPath, objectToString);
+  //push successful answer to console
+  console.table(`User created successfully with id: ${newUserObj.id}`);
 };
