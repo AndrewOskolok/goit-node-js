9fbae0728c36151fdb163ea0251c6d810c5e8a51
Oscolok <61433685+Oscolok@users.noreply.github.com>
2020-09-16 14:11:07 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -42,7 +42,7 @@ exports.Server = class Server {
 
   startListening() {
     this.app.listen(process.env.PORT, () => {
-      console.log("Started listening on port", process.env.PORT);
+      console.log("Started listening on port", 3000);
     });
   }
 };
